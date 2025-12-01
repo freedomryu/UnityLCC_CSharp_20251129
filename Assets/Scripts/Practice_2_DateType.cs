ae10efaf6a672536bbf12bcb56414123975d18a6
freedomryu <dn910402@gmail.com>
2025-12-01 19:09:02 +0800

    Update 作業練習 變數和資料類型 修改2

--- Assets/Scripts/Practice_2_DateType.cs
+++ Assets/Scripts/Practice_2_DateType.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 public class Practice_2_DateType :MonoBehaviour
 {
     public float i1 = 7.7f;
-    public double i2 = 0.12345f;
+    public double i2 = 0.12345d;
     public decimal i3 = 1.23456789m;
 }
