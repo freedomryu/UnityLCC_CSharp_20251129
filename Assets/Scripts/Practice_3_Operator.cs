af6ed04828a090752aabbc854127fdbeb4275909
freedomryu <dn910402@gmail.com>
2025-12-05 11:09:51 +0800

    Update 作業運算子&方法

---
+++ Assets/Scripts/Practice_3_Operator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Practice_3_Operator:MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log(MathQuestion(6 / 2 * (1 + 2)));
+    }
+    private float MathQuestion(float mathProblem)
+    {
+        return mathProblem;
+    }
+}
---
+++ Assets/Scripts/Practice_4_Function.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 函式
+/// </summary>
+public class Practice_4_Function:MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log($"<color=#3f3>布林值:{returnTrue()}</color>"); //True
+        Debug.Log($"<color=#3f3>布林值:{returnFales()}</color>"); //False
+    }
+    /// <summary>
+    /// 布林值true
+    /// </summary>
+    /// <returns>true</returns>
+    private bool returnTrue ()
+    {
+        return true;
+    }
+    /// <summary>
+    /// 布林值false
+    /// </summary>
+    /// <returns>false</returns>
+    private bool returnFales()
+    {
+        return false;
+    }
+}
