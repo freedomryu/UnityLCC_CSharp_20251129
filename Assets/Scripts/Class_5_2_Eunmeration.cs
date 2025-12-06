9e5273ca35ade3809e803e237397483a8ee7bac2
freedomryu <dn910402@gmail.com>
2025-12-06 23:28:00 +0800

    Update 認識switch和列舉

---
+++ Assets/Scripts/Class_5_2_Eunmeration.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace furi
+{
+    /// <summary>
+    /// 列舉:enumeration C# 內關鍵字為簡寫 enum
+    /// </summary>
+    public class Class_5_2_Eunmeration:MonoBehaviour
+    {
+        #region 認識列舉基本用法
+        // 列舉:定義下拉選單的選項(通常都是單選)
+        // 語法:
+        // 修飾詞 關鍵字 enum 列舉名稱{列舉選項}
+        // 定義一個列舉 名稱叫做季節
+        // 列舉都是數值 預設從0開始
+        private enum Season
+        {
+            //定義列舉的選項
+            Spring, Summer, Autium, Winter
+        }
+        // 宣告變數 類型為季節列舉,預設值為夏天
+        [SerializeField, Header("季節")]
+        private Season season = Season.Spring;
+
+        private void Awake()
+        {
+            //取得列舉值
+            Debug.Log(season);
+            //取得列舉整數值
+            Debug.Log((int)season);
+
+            //取得修改後列舉值
+            season = Season.Winter;
+            Debug.Log(season);
+            //取得修改後列舉整數值
+            Debug.Log((int)season);
+            //透過數值設定列舉
+            season = (Season)2;
+            Debug.Log(season); 
+            
+        }
+        #endregion
+        /// <summary>
+        /// 道具
+        /// </summary>
+        private enum Item
+        {
+            None = 0, Coin = 1, BlueWater = 15, Chicken = 20
+        }
+        [SerializeField,Header("道具")]
+        private Item item = Item.Chicken;
+
+        // 執行順序 Awake > Start > Update
+        // 開始事件 : 在喚醒事件後執行一次
+        private void Start()
+        {
+            Debug.Log(item);
+            Debug.Log((int)item);
+        }
+    }
+}
+
--- Assets/Scripts/Class_5_Selection.cs
+++ Assets/Scripts/Class_5_Selection.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Drawing;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 // 命名空間:
@@ -18,7 +19,10 @@ namespace furi
         private bool isOpen;
         [SerializeField, Header("分數"),Range(0,100)]
         private int score = 100;
-
+        [SerializeField, Header("武器")]
+        private string weapon;
+        [SerializeField, Header("血量"),Range(0,100)]
+        private float hp = 100;
         private void Awake()
         {
             #region if判斷式基本陳述式用法
@@ -35,11 +39,11 @@ namespace furi
             } 
             #endregion
 
-        }
+        }      
         private void Update()
         {
             Debug.Log("<color=#79f>更新事件</color>");
-            
+            #region 判斷式if
             if (isOpen)
             {
                 Debug.Log("<color=#93f>已經開門</color>");
@@ -48,15 +52,74 @@ namespace furi
             {
                 Debug.Log("<color=#93f>已經關門</color>");
             }
-
+            // 比較運算子、邏輯運算子結果為布林值
+            // if會在最上方 else會在最下方 中間可以多個 else if
             if (score >= 60)
             {
-                Debug.Log("<color=#3f3>你及格了</color>");
+                Debug.Log("<color=#f96>你及格了</color>");
+            }
+            else if (score > 20)
+            {
+                Debug.Log("<color=#f9a>你不及格,但是可以補考補救</color>");
             }
             else
             {
                 Debug.Log("<color=#3f3>你不及格</color>");
             }
+            #endregion
+
+            #region 判斷式switch
+            // switch 判斷式
+            // switch (要判斷的值) {陳述式}
+            switch (weapon)
+            {
+                // case 值:
+                // 當判斷職等於會執行這裡
+                // break; 跳出判斷是
+                // 如果武器等語小刀,攻擊力等於 20
+                // 蝴蝶刀與小刀一樣
+                case "蝴蝶刀":
+                case "小刀":
+                    Debug.Log("<color=#ff3>攻擊力:20</color>");
+                    break;
+                case "美工刀":
+                    Debug.Log("<color=#ff3>攻擊力:10</color>");
+                    break;
+                case "屠龍刀":
+                    Debug.Log("<color=#ff3>攻擊力:436</color>");
+                    break;
+                // 當weapon的值不等於上面所有值執行
+                default:
+                    Debug.Log("<color=#ff3>這是不能使用的武器</color>");
+                    break;
+
+            }
+            #endregion
+
+            #region 練習區
+            switch (hp)
+            {
+                case >= 80:
+                    Debug.Log("<color=#07e82c>血量安全</color>");
+                    break;
+
+                case >= 60:
+                    Debug.Log("<color=#e7f7be>健康狀態有狀況</color>");
+                    break;
+
+                case >= 40:
+                    Debug.Log("<color=#f7c8a3>警告，快喝水</color>");
+                    break;
+
+                case >= 1:
+                    Debug.Log("<color=#ff0808>快死掉</color>");
+                    break;
+
+                default:
+                    Debug.Log("<color=#ff0808>你已經死了</color>");
+                    break;
+            } 
+            #endregion
         }
     }
 
