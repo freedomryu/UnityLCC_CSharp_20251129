81240aa5245c85d8b61a99585b9a310b7be86c54
freedomryu <dn910402@gmail.com>
2025-12-10 18:08:55 +0800

    Update 作業與迴圈

---
+++ Assets/Scripts/Practice_5_Swtich.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Practice_5_Swtich : MonoBehaviour
+{
+    [SerializeField, Header("寫量"),Range(0,100) ]
+    private float hp = 100;
+
+    private void Update()
+    {
+        switch (hp)
+        {
+            case >=80:
+                Debug.Log($"血量安全");
+            break;
+            case >= 60:
+                Debug.Log($"健康狀態有狀況");
+                break;
+            case >= 40:
+                Debug.Log($"警告，快喝水");
+                break;
+            case >= 10:
+                Debug.Log($"快死掉了");
+                break;
+            case <= 0:
+                Debug.Log($"你已經死了");
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Practice_6_Array.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Practice_6_Array :MonoBehaviour
+{
+    public string[,,] character =
+    {
+        {{"大岩蛇","超夢" },{ "皮卡丘","波加曼"},{"雷丘","水伊布" } },
+        {{"緞帶肥肥","藍水靈" },{ "泡泡魚","蒙面河豚"},{"木妖","菇菇寶貝" }},
+        {{"蓋倫","好運姐" },{ "凱爾","雷玟"},{"艾希","李星" } }
+    };
+
+    private void Start()
+    {
+        Debug.Log($"<color=#ffcccc>{character[0,1,0]}</color>");
+        Debug.Log($"<color=#ff9966>{character[1, 2, 1]}</color>");
+        Debug.Log($"<color=#00cc44>{character[2, 0, 0]}</color>");
+        Debug.Log($"<color=#00cc44>{character[2, 0, 0]}</color>");
+    }
+}
