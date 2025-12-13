d504f64d381dbfcd5c9c0b86ed488bd778559e5c
freedomryu <dn910402@gmail.com>
2025-12-14 00:45:27 +0800

    Update 結構

--- Assets/Scripts/Class_8_1_Class.cs
+++ Assets/Scripts/Class_8_1_Class.cs
@@ -11,11 +11,13 @@ namespace furi
         private void Awake()
         {
             // 實體化NPC並儲存在npcJack 變數內
-            Class_8_1_Npc npcJack = new Class_8_1_Npc();
-            Class_8_1_Npc npcFuri = new Class_8_1_Npc("furi");
+            Class_8_1_Npc npcJack = new Class_8_1_Npc("捷克");
+            Class_8_1_Npc npcFuri = new Class_8_1_Npc("furi","你好");
 
             npcJack.LogName();
             npcFuri.LogName();
+            npcJack.Talk();
+            npcFuri.Talk();
         }
     }
 }
